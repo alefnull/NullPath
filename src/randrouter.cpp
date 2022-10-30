f9afc439fcf7b48d4ba54c92a3e36429cf926a4c
patheros <patheros@gmail.com>
2022-10-30 08:02:52 -0500

    Added in Entangled Panel

--- src/randrouter.cpp
+++ src/randrouter.cpp
@@ -1,5 +1,7 @@
 #include "plugin.hpp"
+#include "widgets.hpp"
 
+#define SIGNAL_COUNT 9
 
 struct Randrouter : Module {
 	enum ParamId {
@@ -11,11 +13,14 @@ struct Randrouter : Module {
 	enum InputId {
 		CLOCK_INPUT,
 		RESET_INPUT,
-		ENUMS(SIGNAL_INPUT, 9),
+		ENUMS(SIGNAL_INPUT, SIGNAL_COUNT),
+		CHANNELS_CV_INPUT, //TODO Implement
+		MODE_CV_INPUT, //TODO Implement
+		ENTROPY_CV_INPUT, //TODO Implement
 		INPUTS_LEN
 	};
 	enum OutputId {
-		ENUMS(SIGNAL_OUTPUT, 9),
+		ENUMS(SIGNAL_OUTPUT, SIGNAL_COUNT),
 		OUTPUTS_LEN
 	};
 	enum LightId {
@@ -51,14 +56,14 @@ struct Randrouter : Module {
 
 	dsp::SchmittTrigger clock_trigger;
 	dsp::SchmittTrigger reset_trigger;
-	int output_map[9] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+	int output_map[SIGNAL_COUNT] = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 	int triplet_swap[3][3] = { { 1, 0, 2 }, { 2, 1, 0 }, { 0, 2, 1 } };
 	int triplet_randomize[5][3] = { { 0, 2, 1 }, { 1, 0, 2 }, { 1, 2, 0 }, { 2, 0, 1 }, { 2, 1, 0 } };
 	bool mono = true;
 
 	int random_index() {
 		if (mono) {
-			return random::u32() % 9;
+			return random::u32() % SIGNAL_COUNT;
 		}
 		else {
 			return (random::u32() % 5) * 2;
@@ -96,10 +101,10 @@ struct Randrouter : Module {
 			output_map[r2] = index_1;
 		}
 		else if (entropy == 2) { // High Entropy - Randomize
-			for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < SIGNAL_COUNT; i++) {
 				output_map[i] = i;
 			}
-			for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < SIGNAL_COUNT; i++) {
 				int r = random_index();
 				int temp = output_map[i];
 				output_map[i] = output_map[r];
@@ -126,12 +131,12 @@ struct Randrouter : Module {
 		else if (entropy == 1) { // Low Entropy - Shunt Up
 			int max_index = random_index();
 			for (int i = 0; i < max_index; i++) {
-				output_map[i] = (output_map[i] - 1 + 9) % 9;
+				output_map[i] = (output_map[i] - 1 + SIGNAL_COUNT) % SIGNAL_COUNT;
 			}
 		}
 		else if (entropy == 2) { // High Entropy - Rotate Up
 			for (int i = 0; i < 8; i++) {
-				output_map[i] = (output_map[i] - 1 + 9) % 9;
+				output_map[i] = (output_map[i] - 1 + SIGNAL_COUNT) % SIGNAL_COUNT;
 			}
 		}
 	}
@@ -154,12 +159,12 @@ struct Randrouter : Module {
 		else if (entropy == 1) { // Low Entropy - Shunt Down
 			int max_index = random_index();
 			for (int i = 0; i < max_index; i++) {
-				output_map[i] = (output_map[i] + 1) % 9;
+				output_map[i] = (output_map[i] + 1) % SIGNAL_COUNT;
 			}
 		}
 		else if (entropy == 2) { // High Entropy - Rotate Down
 			for (int i = 0; i < 8; i++) {
-				output_map[i] = (output_map[i] + 1) % 9;
+				output_map[i] = (output_map[i] + 1) % SIGNAL_COUNT;
 			}
 		}
 	}
@@ -185,7 +190,7 @@ struct Randrouter : Module {
 		}
 		else if (entropy == 2) { // High Entropy - Blast
 			int r = random_index();
-			for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < SIGNAL_COUNT; i++) {
 				output_map[i] = r;
 			}
 		}
@@ -197,7 +202,7 @@ struct Randrouter : Module {
 				const int choices_mono[5] = { 0, 2, 4, 6, 8 };
 				int index = random_index(choices_mono);
 				output_map[index] = index;
-				if (index + 1 < 9) {
+				if (index + 1 < SIGNAL_COUNT) {
 					output_map[index + 1] = index + 1;
 				}
 			}
@@ -205,7 +210,7 @@ struct Randrouter : Module {
 				const int choices_stereo[3] = { 0, 4, 8 };
 				int index = random_index(choices_stereo);
 				output_map[index] = index;
-				if (index + 2 < 9) {
+				if (index + 2 < SIGNAL_COUNT) {
 					output_map[index + 2] = index + 2;
 				}
 			}
@@ -271,7 +276,7 @@ struct Randrouter : Module {
 					}
 				}
 				else {
-					for (int i = 6; i < 9; i++) {
+					for (int i = 6; i < SIGNAL_COUNT; i++) {
 						output_map[i] = i;
 					}
 				}
@@ -338,7 +343,7 @@ struct Randrouter : Module {
 					output_map[2] = prev_input_1;
 					output_map[4] = prev_input_2;
 				}
-				for (int i = 6; i < 9; i++) {
+				for (int i = 6; i < SIGNAL_COUNT; i++) {
 					int r = random::uniform() * 6;
 					int* row = triplet_randomize[r];
 					int prev_input_0 = output_map[6 + row[0]];
@@ -360,7 +365,7 @@ struct Randrouter : Module {
 		bool reset = reset_trigger.process(inputs[RESET_INPUT].getVoltage());
 
 		if (reset) {
-			for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < SIGNAL_COUNT; i++) {
 				output_map[i] = i;
 			}
 		}
@@ -389,12 +394,12 @@ struct Randrouter : Module {
 		}
 
 		if (mono) {
-			for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < SIGNAL_COUNT; i++) {
 				outputs[SIGNAL_OUTPUT + i].setVoltage(inputs[SIGNAL_INPUT + output_map[i]].getVoltage());
 			}
 		}
 		else {
-			for (int i = 0; i < 9; i += 2) {
+			for (int i = 0; i < SIGNAL_COUNT; i += 2) {
 				int index = 2 * std::floor(output_map[i] / 2);
 				bool i_is_8 = i == 8;
 				bool index_is_8 = index == 8;
@@ -437,70 +442,157 @@ struct Randrouter : Module {
 
 
 struct RandrouterWidget : ModuleWidget {
+
+	struct RoutingWidget : widget::Widget{
+		Randrouter* module;
+
+		Vec ins[SIGNAL_COUNT];
+		Vec outs[SIGNAL_COUNT];
+
+		void initalize(){
+			Rect b = box.zeroPos();
+			float div = SIGNAL_COUNT - 1;
+			for(int si = 0; si < SIGNAL_COUNT; si ++){
+				ins[si] = b.interpolate(Vec(0,si/div));
+				outs[si] = b.interpolate(Vec(1,si/div));
+			}
+		}
+
+		void draw(const DrawArgs& args) override {
+			drawLayer(args,0);
+		}
+
+		void drawLayer(const DrawArgs& args, int layer) override {
+
+			if (layer == 0){
+
+				const int DEFAULT_OUTPUT_MAP[SIGNAL_COUNT] = { 5, 6, 7, 4, 0, 8, 2, 3, 1 };
+				const NVGcolor LINE_COLOR = nvgRGBA(0x97, 0xf7, 0xf9, 0x80);
+				const float CONST_RND[SIGNAL_COUNT][SIGNAL_COUNT][4] = {{{0.799f,0.199f,0.007f,0.023f},{0.158f,0.268f,0.887f,0.505f},{0.829f,0.654f,0.756f,0.280f},{0.833f,0.826f,0.479f,0.340f},{0.624f,0.468f,0.544f,0.841f},{0.328f,0.930f,0.496f,0.966f},{0.258f,0.022f,0.003f,0.742f},{0.257f,0.516f,0.592f,0.339f},{0.676f,0.857f,0.465f,0.965f}},{{0.889f,0.320f,0.343f,0.212f},{0.361f,0.072f,0.290f,0.173f},{0.520f,0.152f,0.802f,0.263f},{0.076f,0.900f,0.643f,0.085f},{0.662f,0.072f,0.010f,0.087f},{0.345f,0.046f,0.558f,0.579f},{0.716f,0.336f,0.997f,0.374f},{0.489f,0.861f,0.283f,0.961f},{0.424f,0.954f,0.526f,0.695f}},{{0.552f,0.922f,0.571f,0.014f},{0.910f,0.770f,0.263f,0.045f},{0.763f,0.509f,0.397f,0.620f},{0.914f,0.642f,0.129f,0.849f},{0.534f,0.806f,0.559f,0.353f},{0.657f,0.587f,0.647f,0.085f},{0.348f,0.417f,0.297f,0.695f},{0.652f,0.898f,0.245f,0.241f},{0.368f,0.207f,0.884f,0.593f}},{{0.662f,0.684f,0.716f,0.360f},{0.431f,0.149f,0.519f,0.267f},{0.563f,0.271f,0.630f,0.576f},{0.547f,0.195f,0.629f,0.383f},{0.113f,0.156f,0.088f,0.653f},{0.525f,0.489f,0.227f,0.357f},{0.058f,0.963f,0.232f,0.563f},{0.165f,0.934f,0.515f,0.145f},{0.635f,0.224f,0.684f,0.383f}},{{0.447f,0.363f,0.661f,0.121f},{0.569f,0.888f,0.141f,0.582f},{0.408f,0.779f,0.384f,0.569f},{0.339f,0.832f,0.716f,0.360f},{0.240f,0.173f,0.669f,0.867f},{0.545f,0.315f,0.938f,0.286f},{0.567f,0.911f,0.774f,0.250f},{0.153f,0.179f,0.319f,0.237f},{0.616f,0.110f,0.682f,0.270f}},{{0.559f,0.642f,0.873f,0.179f},{0.844f,0.514f,0.780f,0.954f},{0.461f,0.076f,0.289f,0.694f},{0.924f,0.382f,0.763f,0.522f},{0.479f,0.053f,0.534f,0.423f},{0.281f,0.580f,0.489f,0.320f},{0.110f,0.895f,0.625f,0.666f},{0.467f,0.428f,0.513f,0.714f},{0.641f,0.788f,0.128f,0.733f}},{{0.024f,0.782f,0.744f,0.810f},{0.998f,0.316f,0.414f,0.147f},{0.259f,0.920f,0.687f,0.446f},{0.563f,0.716f,0.039f,0.268f},{0.941f,0.254f,0.918f,0.375f},{0.787f,0.783f,0.158f,0.166f},{0.925f,0.750f,0.355f,0.509f},{0.039f,0.778f,0.073f,0.806f},{0.369f,0.754f,0.652f,0.596f}},{{0.767f,0.822f,0.813f,0.561f},{0.530f,0.280f,0.308f,0.498f},{0.357f,0.570f,0.693f,0.373f},{0.367f,0.433f,0.238f,0.067f},{0.537f,0.888f,0.638f,0.080f},{0.553f,0.825f,0.200f,0.405f},{0.019f,0.037f,0.568f,0.700f},{0.405f,0.365f,0.437f,0.348f},{0.950f,0.463f,0.474f,0.147f}},{{0.110f,0.736f,0.628f,0.519f},{0.534f,0.808f,0.994f,0.545f},{0.842f,0.059f,0.038f,0.157f},{0.740f,0.470f,0.208f,0.379f},{0.785f,0.450f,0.909f,0.500f},{0.810f,0.161f,0.066f,0.958f},{0.926f,0.403f,0.393f,0.354f},{0.338f,0.901f,0.998f,0.533f},{0.111f,0.584f,0.872f,0.080f}}};
+
+				nvgStrokeColor(args.vg, LINE_COLOR);
+				nvgLineCap(args.vg, NVG_ROUND);
+				nvgMiterLimit(args.vg, 2.f);
+				nvgStrokeWidth(args.vg, 2.f);
+
+				bool stereo = false;
+				if(module != NULL){
+					stereo = !module->mono;
+				}
+
+				//Draw Stero Connectors
+				if(stereo){
+					for(int si = 0; si < 8; si += 2){
+						drawConnector(args,ins[si],ins[si+1]);
+						drawConnector(args,outs[si],outs[si+1]);
+					}
+				}
+
+			
+				for(int oi = 0; oi < SIGNAL_COUNT; oi += (stereo ? 2 : 1)){
+					int ii = module == NULL ? DEFAULT_OUTPUT_MAP[oi] : module->output_map[oi];
+
+					Vec in = ins[ii];
+					Vec out = outs[oi];
+
+					if(stereo){
+						if(ii+1 < SIGNAL_COUNT) in = in.plus(ins[ii+1]).mult(0.5f);
+						if(oi+1 < SIGNAL_COUNT) out = out.plus(outs[oi+1]).mult(0.5f);
+					}
+
+					int di = std::abs(ii - oi);
+
+					if(di <= 1){
+						//Draw Straight Line
+						nvgBeginPath(args.vg);
+						nvgMoveTo(args.vg, VEC_ARGS(in));
+						nvgLineTo(args.vg, VEC_ARGS(out));
+						nvgStroke(args.vg);
+					}else{
+						Vec m1;
+						Vec m2;
+
+						Vec d = out.minus(in);
+
+						float h1 = 0.33f + 0.2f * (CONST_RND[oi][ii][0] * 2.f - 1);
+						float h2 = 0.66f + 0.2f * (CONST_RND[oi][ii][1] * 2.f - 1);
+
+						m1 = in.plus(d.mult(h1));
+						m2 = in.plus(d.mult(h2));
+
+						float v1 = 0.5f + 0.5f * (CONST_RND[oi][ii][2] * 2.f - 1);
+						float v2 = 0.5f + 0.5f * (CONST_RND[oi][ii][3] * 2.f - 1);
+
+						m1.y = m1.y * (1-v1) + v1 * in.y;
+						m2.y = m2.y * (1-v2) + v2 * out.y;
+
+						nvgBeginPath(args.vg);
+						nvgMoveTo(args.vg, VEC_ARGS(in));
+						nvgLineTo(args.vg, VEC_ARGS(m1));
+						nvgLineTo(args.vg, VEC_ARGS(m2));
+						nvgLineTo(args.vg, VEC_ARGS(out));
+						nvgStroke(args.vg);
+					}
+				}
+			}	
+		}
+
+		void drawConnector(const DrawArgs& args, Vec a, Vec b){
+			nvgBeginPath(args.vg);
+			nvgMoveTo(args.vg, VEC_ARGS(a));
+			nvgLineTo(args.vg, VEC_ARGS(b));
+			nvgStroke(args.vg);
+		}
+
+	};
+
 	RandrouterWidget(Randrouter* module) {
 		setModule(module);
-		setPanel(createPanel(asset::plugin(pluginInstance, "res/randrouter.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/Entangle.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewBlack>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewBlack>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		float x_start = RACK_GRID_WIDTH * 2;
-		float y_start = RACK_GRID_WIDTH * 4;
-		float dx = RACK_GRID_WIDTH * 2;
-		float dy = RACK_GRID_WIDTH * 2;
+		addParam(createParamCentered<CKSSThree>(mm2px(Vec(63.404, 28.433)), module, Randrouter::MODE_PARAM));
+		addParam(createParamCentered<CKSSThree>(mm2px(Vec(63.404, 66.81)), module, Randrouter::ENTROPY_PARAM));
+		addParam(createParamCentered<CKSS>(mm2px(Vec(63.404, 99.503)), module, Randrouter::CHANNELS_PARAM));
 
-		float x = x_start;
-		float y = y_start;
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.534, 17.146)), module, Randrouter::SIGNAL_INPUT + 0));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.557, 28.794)), module, Randrouter::SIGNAL_INPUT + 1));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.557, 40.353)), module, Randrouter::SIGNAL_INPUT + 2));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.557, 51.869)), module, Randrouter::SIGNAL_INPUT + 3));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.534, 63.448)), module, Randrouter::SIGNAL_INPUT + 4));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.534, 75.023)), module, Randrouter::SIGNAL_INPUT + 5));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.557, 86.671)), module, Randrouter::SIGNAL_INPUT + 6));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.557, 98.247)), module, Randrouter::SIGNAL_INPUT + 7));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(21.557, 109.746)), module, Randrouter::SIGNAL_INPUT + 8));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(8.129, 58.382)), module, Randrouter::CLOCK_INPUT));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(8.151, 76.546)), module, Randrouter::RESET_INPUT));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(63.404, 44.207)), module, Randrouter::MODE_CV_INPUT));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(63.381, 78.229)), module, Randrouter::ENTROPY_CV_INPUT));
+		addInput(createInputCentered<NP::InPort>(mm2px(Vec(63.404, 109.741)), module, Randrouter::CHANNELS_CV_INPUT));
 
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::CLOCK_INPUT));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::RESET_INPUT));
-		y += dy;
-		addParam(createParamCentered<CKSS>(Vec(x, y), module, Randrouter::CHANNELS_PARAM));
-		y += dy;
-		addParam(createParamCentered<CKSSThree>(Vec(x, y), module, Randrouter::MODE_PARAM));
-		y += dy;
-		addParam(createParamCentered<CKSSThree>(Vec(x, y), module, Randrouter::ENTROPY_PARAM));
-		y -= dy * 3;
-		x += dx;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 0));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 1));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 2));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 3));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 4));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 5));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 6));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 7));
-		y += dy;
-		addInput(createInputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_INPUT + 8));
-		y -= dy * 8;
-		x += dx;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 0));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 1));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 2));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 3));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 4));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 5));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 6));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 7));
-		y += dy;
-		addOutput(createOutputCentered<PJ301MPort>(Vec(x, y), module, Randrouter::SIGNAL_OUTPUT + 8));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.567, 17.286)), module, Randrouter::SIGNAL_OUTPUT + 0));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.563, 28.87)), module, Randrouter::SIGNAL_OUTPUT + 1));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.567, 40.437)), module, Randrouter::SIGNAL_OUTPUT + 2));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.563, 52.021)), module, Randrouter::SIGNAL_OUTPUT + 3));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.563, 63.596)), module, Randrouter::SIGNAL_OUTPUT + 4));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.563, 75.172)), module, Randrouter::SIGNAL_OUTPUT + 5));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.563, 86.747)), module, Randrouter::SIGNAL_OUTPUT + 6));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.563, 98.323)), module, Randrouter::SIGNAL_OUTPUT + 7));
+		addOutput(createOutputCentered<NP::OutPort>(mm2px(Vec(49.567, 109.89)), module, Randrouter::SIGNAL_OUTPUT + 8));
+
+		addChild(createRoutingWidget(module,mm2px(Vec(21.557, 17.142)),mm2px(Vec(28.006, 92.756))));
+	}
+
+	RoutingWidget* createRoutingWidget(Randrouter* module, Vec pos, Vec size){
+		RoutingWidget* display = createWidget<RoutingWidget>(pos);
+		display->module = module;
+		display->box.size = size;
+		display->initalize();
+		return display;
 	}
 };
 
