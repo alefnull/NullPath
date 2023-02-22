b46848bee85e27d229b8775d4ee908f02bb56374
alefnull <nullalef@gmail.com>
2023-02-22 17:09:12 -0500

    Entangle: fix stereo index mismatch

--- src/randrouter.cpp
+++ src/randrouter.cpp
@@ -656,7 +656,7 @@ struct Randrouter : Module {
 		else {
 			for (int ii = 0; ii < SIGNAL_COUNT; ii += 2) {
 				int oi = 2 * std::floor(output_map[ii] / 2);
-				bool ii_is_8 = oi == 8;
+				bool ii_is_8 = ii == 8;
 				bool oi_is_8 = oi == 8;
 				if (ii_is_8) {
 					if (oi_is_8) {
